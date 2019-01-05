e000251a82ac003ef4a701d5f592cafa5f7c6320
Carlos Andres Benavides <cabenavidesd@misena.edu.co>
2019-01-04 23:49:44 -0500

    chore: add search component

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { IMovie } from '../../interfaces/movie';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchWord: string;
+  movies: IMovie[];
+
+  constructor(private moviesService: MoviesService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.searchWord = params.word;
+      this.searchMovie();
+    });
+  }
+
+  searchMovie() {
+    this.moviesService.getMovieByWord(this.searchWord).subscribe(data => {
+      this.movies = data.slice(10);
+    }, err => {
+      console.log('ERROR ', err);
+    });
+  }
+
+  goToMovie(idMovie) {
+    this.router.navigate(['movie', idMovie, 'search', this.searchWord]);
+  }
+
+}
